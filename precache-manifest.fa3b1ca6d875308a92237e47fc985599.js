da36a9f3a4c2b5c4c26a769f10c6f05d78018c2b
= <=>
2019-01-25 21:38:35 +0100

    Updates

---
+++ precache-manifest.fa3b1ca6d875308a92237e47fc985599.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b2d8c3c839a93fb5b7d",
+    "url": "/-Donor-Shop-/static/js/runtime~main.3b2d8c3c.js"
+  },
+  {
+    "revision": "7f074f3f427a3c2ebf9f",
+    "url": "/-Donor-Shop-/static/js/main.7f074f3f.chunk.js"
+  },
+  {
+    "revision": "0df2649e1712906806b5",
+    "url": "/-Donor-Shop-/static/js/1.0df2649e.chunk.js"
+  },
+  {
+    "revision": "7f074f3f427a3c2ebf9f",
+    "url": "/-Donor-Shop-/static/css/main.5ff1ec6a.chunk.css"
+  },
+  {
+    "revision": "0df2649e1712906806b5",
+    "url": "/-Donor-Shop-/static/css/1.f42ec530.chunk.css"
+  },
+  {
+    "revision": "d3ef403ca5b34d785ea8d6cc1a75728c",
+    "url": "/-Donor-Shop-/index.html"
+  }
+];
